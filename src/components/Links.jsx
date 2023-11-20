f4e46dbe649d6185f80d87b7a7c90fe7ea2978f2
Ansh Gupta <ansh.go2002@gmail.com>
2023-11-20 20:49:18 +0530

    changes made#2

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -57,7 +57,7 @@ const Links = () => {
 
   return (
     <motion.div className="shadow-lg shadow-black w-[400px] bg-[#000814] h-screen flex flex-col justify-center items-center font-bold text-2xl gap-y-[15px]" variants={variants}>
-        <motion.img variants={imageVariants} className="w-[120px] h-[120px] mb-10 shadow-lg" src="https://github.com/Ansh2002Gupta/My-Portfolio/blob/gh-pages/me.png" alt="me" />
+        <motion.img variants={imageVariants} className="w-[120px] h-[120px] mb-10 shadow-lg" src="https://github.com/Ansh2002Gupta/My-Portfolio/blob/master/public/me.png" alt="me" />
         <svg className="" width="240px" height="240px" viewBox="-2.4 -2.4 28.80 28.80" fill="none" xmlns="http://www.w3.org/2000/svg" transform="rotate(0)" style={{position: "absolute", top: "45px", left: "80px"}}>
           <g id="SVGRepo_bgCarrier" style={{strokeWidth: "0"}}></g>
           <g id="SVGRepo_tracerCarrier" style={{strokeLineCap: "round", strokeLineJoin: "round", stroke: "#CCCCCC", strokeWidth: "0.288"}}></g>
@@ -70,7 +70,7 @@ const Links = () => {
           {item}
         </motion.a>
       ))}
-      <motion.a variants={itemVariants} whileHover={{backgroundColor: "#FFC300", color: "#000814", transition: {duration: 0.5, type: "spring"}}} className="px-2 py-3 text-bold rounded-md font-['Tajawal'] text-[#FFC300] bg-[#000814]" href="https://github.com/Ansh2002Gupta/My-Portfolio/blob/gh-pages/Resume-%20Ansh%20Gupta.pdf" download>
+      <motion.a variants={itemVariants} whileHover={{backgroundColor: "#FFC300", color: "#000814", transition: {duration: 0.5, type: "spring"}}} className="px-2 py-3 text-bold rounded-md font-['Tajawal'] text-[#FFC300] bg-[#000814]" href="https://github.com/Ansh2002Gupta/My-Portfolio/blob/master/public/Resume-%20Ansh%20Gupta.pdf" download>
               My Resume
       </motion.a>
     </motion.div>
